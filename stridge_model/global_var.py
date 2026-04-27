c3a3ebaf4842301f89f11f7410c739fc0ce28b67
Minnie Soo <wymsoo@connect.ust.hk>
2026-04-27 14:57:13 +0800

    update global_var

--- stridge_model/global_var.py
+++ stridge_model/global_var.py
@@ -1,6 +1,6 @@
-global dt, Nx, Ny, dx, dy, Re, rho, G, D, max_ts, H, L, viscosity, epsilon, endtime, time_res
+global dt, Nx, Ny, dx, dy, Re, rho, G, D, max_ts, H, L, viscosity, epsilon
 rho = 1000
-viscosity = 10 #viscosity of honey Pa s^-1
+viscosity = 10 #viscosity of honey Pa s^-1 
 Re = rho/viscosity*0.1*0.05 #= 14*0.05 = 0.7
 G = 9.81
 D = 0.05 # radius: 1cm
@@ -8,11 +8,11 @@ L = 0.05 # length: 1m
 # Parameters
 dt = 0.0001 #vdt ~0.1*0.01 = 0.001
 H = 1
-max_ts = 100000
-Nx = 11
-Ny = 11
+max_ts = 10000
+Nx = 31
+Ny = 31
 dx = L / Nx # 0.005
 dy = D / Ny
 epsilon = 1e-15
-endtime = 0.255 # 0.1milliseecond
-time_res = 50 # milliseconds
+
+
